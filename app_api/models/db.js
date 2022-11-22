6cc7a2ba77216b2508a2a9712a47e9181f4b7b5b
hilmidmrr <hilmihsandemir70@gmail.com>
2022-11-22 19:32:13 +0300

    olusturuldu

---
+++ app_api/models/db.js
@@ -0,0 +1,28 @@
+var mongoose = require( 'mongoose' ); 
+require("./mekansema");
+var dbURI = "mongodb+srv://hidmr:12345@mekanbul.wuvfmqc.mongodb.net/?retryWrites=true&w=majority" 
+//var dbURI = "mongodb://localhost/mekanbul"
+
+mongoose.connect(dbURI); 
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+    console.log(msg);
+    callback();
+});
+}
+
+process.on("SIGINT",function(){
+    kapat("Uygulama kapatıldı!",function(){
+        process.exit(0);
+    });
+});
+
+mongoose.connection.on("connected",function(){
+    console.log(dbURI + "   adresteki veri tabanına bağlandı!\n");
+});
+mongoose.connection.on("error",function(){
+    console.log("Bağlantı hatası\n");
+});
+mongoose.connection.on("disconnected",function(){
+    console.log("Bağlantı kesildi!\n");
+});
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,89 +1,84 @@
-var express = require('express');
-var router = express.Router();
-
-const anaSayfa=function(req, res, next) {
-    res.render('anasayfa',
-    {
-    "baslik":"Anasayfa",
-     "sayfabaslik":{
+const anaSayfa=function(req, res) {
+    res.render('anasayfa',{ 
+    //buraları çift tırnakta yaz, json ile ilgili olunca çift tırnak olacakmış
+      "baslik": "Anasayfa",
+      "sayfaBaslik":{
         "siteAd":"MekanBul",
-        "Slogan":"Civardaki Mekanları keşfet!"
-     },   
-     "mekanlar":[
+        "slogan":"Civardaki Mekanları Keşfet!"
+      },
+      "mekanlar":[
         {
-            "ad":"Starbucks",
-            "puan":"3",
-            "adres":"Centrum Garden Avm",
-            "imkanlar":["Dunya Kahveleri","Cay","Kek"],
-            "mesafe":"10km"
+          "ad":"Starbucks",
+          "puan":"3",
+          "adres":"Centrum Garden AVM",
+          "imkanlar":["Dünya Kahveleri","Kurabiyeler"],
+          "mesafe":"10 km"
         },
         {
-            "ad":"Barida Kafe",
-            "puan":"1",
-            "adres":"Sdu batı kampusu",
-            "imkanlar":["Tost","Cay","Kahve"],
-            "mesafe":"1km"
-        },
-        
-     ]
+          "ad":"Taş Kafe",
+          "puan":"4",
+          "adres":"SDÜ Batı Kampüsü",
+          "imkanlar":["Çorba","Kahvaltı"],
+          "mesafe":"1 km"
+        }
+      ]
     }
     );
-}
-const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi',
-    {  
-        "baslik":"Mekan Bilgisi",
+  }
+  
+  const mekanBilgisi=function(req, res) {
+    res.render('mekanbilgisi', 
+      { 
+        'baslik': 'Mekan Bilgisi',
         "mekanBaslik":"Starbucks",
         "mekanDetay":{
-            "ad":"Starbucks",
-            "puan":"3",
-            "adres":"Centrum Garden Avm",
-            "saatler":[
-                {
-                "gunler":"Pazartesi-Cuma",
-                "acilis":"9:00",
-                "kapanis":"23:00",
-                "kapali":false
-                },
-                {
-                    "gunler":"Cumartesi-Pazar",
-                    "acilis":"9:00",
-                    "kapanis":"21:00",
-                    "kapali":false
-                }
-            ],
-            "imkanlar":[
-                "kahve","Çay","Kek"
-            ],
-            "koordinatlar":{
-                "enlem":"37.7",
-                "boylam":"30.5"
+          "ad":"Starbucks",
+          "puan":"3",
+          "adres":"Centrum Garden AVM",
+          "saatler":[
+          {
+            "gunler":"Pazartesi-Cuma",
+            "acilis":"07:00",
+            "kapanis":"23:00",
+            "kapali":false
+          },
+          {
+            "gunler":"Cumartesi-Pazar",
+            "acilis":"09:00",
+            "kapanis":"22:00",
+            "kapali":false
+          }
+        ],
+        "imkanlar":["Dünya Kahveleri","Kurabiyeler"],
+          "koordinatlar":{
+            "enlem":"37.7",
+            "boylam":"30.5"
+          },
+          "yorumlar":[
+            {
+              "yorumYapan":"Hilmi İhsan Demir",
+              "yorumMetni":"Güzeldi",
+              "yorumTarihi":"20 Ekim 2022",
+              "puan":"4"
             },
-            "yorumlar":[
-                {
-                    "yorumYapan":"Emre BULUN",
-                    "yorumMetini":"Berbattı.",
-                    "tarih":"26 Ekim 2022",
-                    "puan":"1"
-                },
-                {
-                    "yorumYapan":"Asım Sinan Yüksel",
-                    "yorumMetini":"Güzeldi",
-                    "tarih":"05 Aralık 2021",
-                    "puan":"5"
-                }
-            ]
+            {
+              "yorumYapan":"Asım Sinan Yüksel",
+              "yorumMetni":"İdare eder.",
+              "yorumTarihi":"20 Ekim 2022",
+              "puan":"3"
+            }
+          ]
         }
-    }
-    
-    );
-}
-const yorumEkle=function(req, res, next) {
-    res.render('yorumekle', { title: 'Yorum Ekle' });
-}
-
-module.exports={
-    anaSayfa,
-    mekanBilgisi,
-    yorumEkle
-}
+      });
+  }
+  
+  const yorumEkle=function(req, res) {
+    res.render('yorumekle', { 'title': 'Yorum Ekle' });
+  }
+  
+  //modules.export içine yazılan dış dunyaya acılır
+  module.exports={
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+  };
