29f98415ff74bc42f776cbcf62b478a5e809042e
hilmidmrr <hilmihsandemir70@gmail.com>
2022-12-08 14:04:22 +0300

    odev8

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,27 +1,28 @@
-var mongoose = require( 'mongoose' ); 
-require("./mekansema");
+var mongoose = require('mongoose' );
+require("./mekansema"); 
 var dbURI = "mongodb+srv://hidmr:12345@mekanbul.wuvfmqc.mongodb.net/mekanbul?retryWrites=true&w=majority"
 
-mongoose.connect(dbURI); 
+mongoose.connect(dbURI);
+
 function kapat(msg,callback){
-    mongoose.connection.close(function(){
+mongoose.connection.close(function(){
     console.log(msg);
     callback();
 });
 }
-
-process.on("SIGINT",function(){
-    kapat("Uygulama kapatıldı!",function(){
+process.on("SIGINT", function () {
+    kapat("uygulama kapatıldı", function(){
         process.exit(0);
     });
 });
 
-mongoose.connection.on("connected",function(){
-    console.log(dbURI + " adresteki veri tabanına bağlandı!\n");
+
+mongoose.connection.on("connected", function(){
+    console.log(dbURI + "adresindeki veritabanına bağlanıldı\n");
 });
-mongoose.connection.on("error",function(){
-    console.log("Bağlantı hatası\n");
+mongoose.connection.on("error", function(){
+    console.log("Baglantı hatası\n");
 });
-mongoose.connection.on("disconnected",function(){
-    console.log("Bağlantı kesildi!\n");
+mongoose.connection.on("disconnected", function(){
+    console.log("baglantı kesildi\n");
 });
